8ee926965fdf6093ee45c19af0d56851b6a0943c
Rohan-shakya <rohanshakya254@gmail.com>
2020-08-26 22:35:44 +0545

    added ui

--- server.js
+++ server.js
@@ -1,3 +1,22 @@
 const express = require('express');
-
 const app = express();
+
+// Port
+const PORT = 3000 || process.env.PORT;
+
+// register view engine
+app.set('view engine', 'ejs');
+
+app.get('/', (req, res) => {
+  const todos = [
+    { id: 1, todo: 'Buying' },
+    { id: 2, todo: 'Washing' },
+  ];
+  res.render('index', { title: 'All todos', todos });
+});
+
+app.use((req, res) => {
+  res.status(404).render('404', { title: '404' });
+});
+
+app.listen(PORT, () => console.log(`Listening to the port : ${PORT}`));
